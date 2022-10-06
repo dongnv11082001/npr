16c7aedaee87b0c0fa5eb0bb8b73c9be8aaf44fa
dongnv11082001 <hiiamgosu4@gmail.com>
2022-10-06 22:46:20 +0700

    source code week 2

---
+++ week2/src/URLConnectionReader.java
@@ -0,0 +1,13 @@
+import java.net.* ;
+import java.io.* ;
+public class URLConnectionReader {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://hanu.vn") ;
+        URLConnection uc = url.openConnection() ;
+        BufferedReader br=new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        String line ;
+        while ((line=br.readLine())!=null)
+            System.out.println(line) ;
+        br.close() ;
+    }
+}
